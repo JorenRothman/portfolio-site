e06480886f082b614bb82192b82425f024dfab36
Joren Rothman <joren@instance.studio>
2024-01-28 18:00:55 +0100

    feat: :chart_with_upwards_trend: Added Umami script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const primaryFont = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={primaryFont.className}>{children}</body>
+            <body className={primaryFont.className}>
+                {children}
+                <Script
+                    src="https://stats.jorenrothman.nl/script.js"
+                    data-website-id="01f84d9c-3a08-4b97-9402-0a9c25d4294c"
+                ></Script>
+            </body>
         </html>
     );
 }
