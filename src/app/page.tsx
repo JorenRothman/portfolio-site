a3a8475a1bec393f2844fee9df74dde22fec9824
Joren Rothman <joren@instance.studio>
2024-01-28 19:40:50 +0100

    Site Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Header from '@/app/_components/Header';
 import Projects from '@/app/_components/Projects';
 import Resume from '@/app/_components/Resume';
+import SiteHeader from '@/components/SiteHeader';
 
 export default function Home() {
     return (
-        <main className="">
-            <Header />
-            <Resume />
-            <Projects />
-        </main>
+        <>
+            <SiteHeader />
+            <main className="">
+                <Header />
+                <Resume />
+                <Projects />
+            </main>
+        </>
     );
 }
