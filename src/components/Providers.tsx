b384f657a3590e8d61aac564739c531276c2c666
Joren Rothman <joren@instance.studio>
2024-01-29 00:00:34 +0100

    Update default theme

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,5 +5,9 @@ import { ThemeProvider } from 'next-themes';
 export default function Providers({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
-    return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+    return (
+        <ThemeProvider defaultTheme="light" attribute="class">
+            {children}
+        </ThemeProvider>
+    );
 }
