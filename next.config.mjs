261a9831dc17b753f44d35b1f07a9996e66ff297
Joren Rothman <joren@instance.studio>
2024-01-30 17:14:01 +0100

    Added standalone option

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 import million from 'million/compiler';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone',
+};
 
 export default million.next(nextConfig, { auto: { rsc: true } });
