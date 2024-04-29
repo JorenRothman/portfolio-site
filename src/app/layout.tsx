055ae9ef4cf6f4f82f4616e03727076d96ad501d
Joren Rothman <joren@instance.studio>
2024-04-29 15:51:16 +0200

    Updated personal site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Karla } from 'next/font/google';
 import './globals.css';
 import Providers from '@/components/Providers';
+import Script from 'next/script';
 
 const primaryFont = Karla({ subsets: ['latin'] });
 
@@ -20,6 +21,12 @@ export default function RootLayout({
         <html lang="en" suppressHydrationWarning>
             <body className={primaryFont.className}>
                 <Providers>{children}</Providers>
+
+                <Script
+                    defer
+                    src="https://stats.jorenrothman.nl/script.js"
+                    data-website-id="6eff5fe2-5adb-4e68-aa2d-45081f2297a5"
+                ></Script>
             </body>
         </html>
     );
