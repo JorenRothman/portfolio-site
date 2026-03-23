2765e46a359990804a5cc1a839ffb74aed83a202
Joren <rothmanjoren@gmail.com>
2026-03-23 09:21:33 +0100

    update

--- src/app/_components/Resume.tsx
+++ src/app/_components/Resume.tsx
@@ -85,7 +85,7 @@ export default function Resume() {
                                 items={['TypeScript', 'JavaScript', 'PHP']}
                             />
                             <SkillGroup
-                                label="Frameworks"
+                                label="Libs & Frams"
                                 items={[
                                     'Next.js',
                                     'React',
